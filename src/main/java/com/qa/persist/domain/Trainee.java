bd2abcc5567c16cbe3e02bfe77c232b96bca317e
husnansultan <husnan.sultan@academytrainee.com>
2019-01-18 14:39:16 +0000

    Joins of tables made

--- src/main/java/com/qa/persist/domain/Trainee.java
+++ src/main/java/com/qa/persist/domain/Trainee.java
@@ -1,9 +1,12 @@
 package com.qa.persist.domain;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Trainee {
@@ -13,14 +16,19 @@ public class Trainee {
 	private Long id;
 	private Long trainee_ID;
 	private String traineeName;
+	
+	@ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "classroom_ID")
+	private Long classroom_ID;
 
 	public Trainee() {
 
 	}
 
-	public Trainee(Long trainee_ID, String traineeName) {
-		this.setTraineeID(trainee_ID);
-		this.setTraineeName(traineeName);
+	public Trainee(Long trainee_ID, String traineeName, Long classroom_ID) {
+		this.trainee_ID = trainee_ID;
+		this.traineeName = traineeName;
+		this.classroom_ID = classroom_ID;
 	}
 
 	@Override
@@ -28,12 +36,12 @@ public class Trainee {
 		return null;
 	}
 
-	public Long getTraineeID() {
-		return trainee_ID;
+	public Long getTrainee_ID() {
+		return id;
 	}
 
-	public void setTraineeID(Long trainee_ID) {
-		this.trainee_ID = trainee_ID;
+	public void setTrainee_ID(Long trainee_ID) {
+		this.id = trainee_ID;
 	}
 
 	public String getTraineeName() {
@@ -42,6 +50,14 @@ public class Trainee {
 
 	public void setTraineeName(String traineeName) {
 		this.traineeName = traineeName;
+	}
+
+	public Long getClassroom_ID() {
+		return classroom_ID;
+	}
+
+	public void setClassroom_ID(Long classroom_ID) {
+		this.classroom_ID = classroom_ID;
 	}
 
 }
